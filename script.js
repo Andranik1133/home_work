c5ea5723f55c4b42d880227b534b90dfa37e0865
Andranik1133 <ando.aleksanyan93@gmail.com>
2024-10-29 20:55:37 +0400

    new Date

--- script.js
+++ script.js
@@ -10,6 +10,19 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300], //շարժումներ
   interestRate: 1.2, //տոկոսադրույք
   pin: 1111, //քորոց
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT',
 };
 
 const account2 = {
@@ -17,23 +30,36 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30], //շարժումներ
   interestRate: 1.5, //տոկոսադրույք
   pin: 2222, //քորոց
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
-const account3 = {
-  owner: 'Exo Paruryan', //սեփականատեր
-  movements: [200, -200, 340, -300, -20, 50, 400, -460], //շարժումներ
-  interestRate: 0.7, //տոկոսադրույք
-  pin: 3333, //քորոց
-};
+// const account3 = {
+//   owner: 'Exo Paruryan', //սեփականատեր
+//   movements: [200, -200, 340, -300, -20, 50, 400, -460], //շարժումներ
+//   interestRate: 0.7, //տոկոսադրույք
+//   pin: 3333, //քորոց
+// };
 
-const account4 = {
-  owner: 'Suren Hakobyan', //սեփականատեր
-  movements: [430, 1000, 700, 50, 90], //շարժումներ
-  interestRate: 1, //տոկոսադրույք
-  pin: 4444, //քորոց
-};
+// const account4 = {
+//   owner: 'Suren Hakobyan', //սեփականատեր
+//   movements: [430, 1000, 700, 50, 90], //շարժումներ
+//   interestRate: 1, //տոկոսադրույք
+//   pin: 4444, //քորոց
+// };
 
-const accounts = [account1, account2, account3, account4]; //հաշիվներ
+const accounts = [account1, account2]; //հաշիվներ
 
 // Elements
 const labelWelcome = document.querySelector('.welcome'); //բարի գալուստ
@@ -61,19 +87,28 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount'); //�
 const inputCloseUsername = document.querySelector('.form__input--user'); //մուտքագրել Փակել օգտվողի անունը
 const inputClosePin = document.querySelector('.form__input--pin'); //մուտքագրել Փակել փին
 
-const displayMovements = function (movements, sort = false) {
+const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
-
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  console.log(acc.movements);
+  const movs = sort
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements;
 
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const date = new Date(acc.movementsDates[i]);
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    const displayDate = `${day}/${month}/${year}`;
 
     const html = `
      <div class="movements__row">
        <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type} </div>
+    <div class="movements__date">${displayDate}</div>
        <div class="movements__value">${mov}💶</div>
       </div>
     `;
@@ -120,13 +155,17 @@ const createUsernames = function (accs) {
 createUsernames(accounts);
 
 const updateUI = function (acc) {
-  displayMovements(acc.movements);
+  displayMovements(acc);
   calcDisplayBalance(acc);
   calcDisplaySummary(acc);
 };
 
 // Event handler
 let currentAccount;
+
+currentAccount = account1;
+updateUI(currentAccount);
+containerApp.style.opacity = 100;
 
 btnLogin.addEventListener('click', function (e) {
   e.preventDefault();
@@ -140,6 +179,17 @@ btnLogin.addEventListener('click', function (e) {
       currentAccount.owner.split(' ')[0]
     }`;
     containerApp.style.opacity = 100;
+
+    // create current date and trime
+
+    const now = new Date();
+    const day = `${now.getDate()}`.padStart(2, 0);
+    const month = `${now.getMonth() + 1}`.padStart(2, 0);
+    const year = now.getFullYear();
+    const hour = `${now.getHours()}`.padStart(2, 0);
+    const min = `${now.getMinutes()}`.padStart(2, 0);
+
+    labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
 
     inputLoginUsername.value = inputLoginPin.value = '';
     inputLoginPin.blur();
@@ -166,6 +216,9 @@ btnTransfer.addEventListener('click', function (e) {
     currentAccount.movements.push(-amount);
     receiverAcc.movements.push(amount);
 
+    currentAccount.movementsDates.push(new Date().toISOString());
+    receiverAcc.movementsDates.push(new Date().toISOString());
+
     // Update UI
     updateUI(currentAccount);
   }
@@ -178,6 +231,8 @@ btnLoan.addEventListener('click', function (e) {
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     currentAccount.movements.push(amount);
+
+    currentAccount.movementsDates.push(new Date().toISOString());
 
     // update UI
     updateUI(currentAccount);
@@ -209,10 +264,10 @@ let sorted = false;
 btnSort.addEventListener('click', function (e) {
   e.preventDefault();
 
-  displayMovements(currentAccount.movements, !sorted)
+  displayMovements(currentAccount.movements, !sorted);
 
   sorted = !sorted;
-})
+});
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
